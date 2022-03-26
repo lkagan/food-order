b4dc9e6bda3c72e604269c9c65a22b06d85142bd
Larry Kagan <larry@superiocity.com>
2022-03-26 15:16:52 -0400

    Meal listing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Header from "./components/Layout/Header";
 import {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  return (
-    <Fragment>
-      <h2>Let's get started!</h2>
-        <Header />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <h2>Let's get started!</h2>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </Fragment>
+    )
 }
 
 export default App;
