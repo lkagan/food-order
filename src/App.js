29b7733e55efc0c51b80f30366bbfd14ab69417c
Larry Kagan <larry@superiocity.com>
2022-03-29 11:16:53 -0400

    Add cart modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Layout/Header";
 import {Fragment} from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
     return (
         <Fragment>
             <h2>Let's get started!</h2>
+            <Cart />
             <Header/>
             <main>
                 <Meals/>
